23a8dcb8941e88d15e048316c8344bb761f44668
Shervon Ceromain <shervonc@gmail.com>
2020-02-09 19:04:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -63,13 +63,13 @@ def movies_with_director_key(name, movies_collection)
 # end
 # new_array
 # end
-ret_hash = []
-  col = 0 
-  while movies_collection[col] do 
-    ret_hash[col]= movie_with_director_name(name, movies_collection[col])  
-    col += 1
+new_array = []
+  i = 0 
+  while movies_collection[i] do 
+    new_array[i]= movie_with_director_name(name, movies_collection[i])  
+    i += 1
   end
-  ret_hash
+  new_array
 end	
 
 
